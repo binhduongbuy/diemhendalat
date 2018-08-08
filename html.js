e85d52117f7cd9f000f10b36456a83a002e9ea0e
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-08-09 00:41:50 +0700

    1

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-5301966823917485',
+    google_ad_client: 'ca-pub-IDHERE',
     enable_page_level_ads: true
   });
   `;
