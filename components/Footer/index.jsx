1cc7b6dadfc4a478570dfa319099891c2271833f
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-08-21 19:50:19 +0700

    1

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import { RouteHandler, Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
-import { config } from 'config'
-import Ink from 'react-ink'
 
 import './style.scss'
 import iconEagle from '../../static/img/svg-icons/nihonsuruinline.svg'
 import iconKamon from '../../static/img/svg-icons/nihonsuruinline.svg'
-import iconOdayaka from '../../static/img/svg-icons/dung.svg'
+import iconBakaDono from '../../static/img/svg-icons/dung.svg'
+import { config } from 'config'
 
 class Footer extends React.Component {
   render() {
-    const {location} = this.props
-    const {route} = this.props
-    const page = route.page.data
 
     return (
       <div className='footer'>
@@ -21,8 +16,6 @@ class Footer extends React.Component {
 
 	<div className='footer__container-floating'>
 
- 
-  
 
 </div>
 
@@ -31,7 +24,7 @@ class Footer extends React.Component {
           </div>
        <div className='footer__left'>
 
-	    <div><img className='footer__powered-svg' src={ prefixLink(iconOdayaka) }/> { config.siteTitle }</div>
+	    <div><img className='footer__powered-svg' src={ prefixLink(iconBakaDono) }/> { config.siteTitle }</div>
 
 
         </div>
@@ -43,10 +36,10 @@ class Footer extends React.Component {
           
 	    
 	     <div className='footer__powered'>
-            <span>Thành viên của </span>
+            <span>Member of the clan </span>
             <a href='https://binhduongbuy.com' className='footer__powered-link' target='_blank'>
               <img className='footer__powered-svg' src={ prefixLink(iconEagle) }/>
-              <span className='footer__powered-fx'>Trương Dũng</span>
+              <span className='footer__powered-fx'>Truong Dung</span>
             </a>
           </div>
 
@@ -64,7 +57,7 @@ class Footer extends React.Component {
             <a href='/tos/privacy'>Privacy</a>
             <a href='/tos'>Terms of service</a>
             <a href='/about' className='is-selected'><img className='footer__powered-svg' src={ prefixLink(iconKamon) }/> Giới thiệu { config.siteTitle }</a>
-	    <a href='https://binhduongbuy.com/'>© Trương Dũng Bình Dương. <strong>{ config.sitePhone }</strong></a>
+	    <a href='https://binhduongbuy.com/'>© Truong Dung Private Enterprise (P.E)</a>
           </nav>
         </div>
         </div>
